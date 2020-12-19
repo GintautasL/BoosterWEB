bb8689aa8f859c4ad5813f3aa7252f22029e19e3
Ignas <ignas.jasonas@telesoftas.com>
2020-12-19 17:16:36 +0200

    lol xD

--- src/components/pages/homepage/component.tsx
+++ src/components/pages/homepage/component.tsx
@@ -17,6 +17,18 @@ export const HomepageComponent: React.FunctionComponent<HomepageProps> = ({
 }) => (
   <div>
     <h1>Eplore nearby graffities</h1>
+    <div className="cards-container">
+    {graffities.map((program) => (
+      <div className="card">
+        <h2> Starting Elo</h2>
+        <h4> {program.starting_elo} </h4>
+        <h2> Target Elo </h2>
+        <h4> {program.target_elo} </h4>
+        <h2> Price </h2>
+        <h4> {program.price} € </h4>
+        </div>
+    ))}
+    </div>
     
     <style jsx>
       {`
@@ -26,6 +38,33 @@ export const HomepageComponent: React.FunctionComponent<HomepageProps> = ({
         h1 {
           margin-top: 0px;
           color: ${color('light', 200)};
+        }
+        h2 {
+          text-align: center;
+          margin-bottom: 5px;
+        }
+        h4 {
+          text-align: center;
+          margin-bottom: 40px;
+          margin-top: 5px;
+        }
+        .card {
+          width: 250px;
+          background-image: radial-gradient(farthest-corner at 40px 40px, rgb(0, 0, 0), rgb(55, 14, 68));
+          color: white;
+          border-radius: 8px;
+          min-height: 340px;
+          margin: 0 10px 20px 10px;
+          cursor: pointer;
+          padding: 8px;
+        }
+        .card:hover {
+          box-shadow: 5px 5px 10px white;
+        }
+        .cards-container {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
         }
       `}
     </style>
